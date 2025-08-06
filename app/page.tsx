a4032f8efb5483694efb983770fd8b1e5d016a35
alexander962 <sanek12011996@gmail.com>
2025-08-06 21:30:42 +0300

    add title, categories and sort-popup

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
-import { Button } from "@/components/ui/button";
+import {Categories, Container, SortPopup, Title} from "@/components/shared";
 
 export default function Home() {
   return (
-    <h1>Главная</h1>
+    <>
+      <Container className='mt-10'>
+        <Title text='Все пиццы' size='lg' className='font-extrabold' />
+
+        <Categories />
+        <SortPopup />
+      </Container>
+    </>
   );
 }
